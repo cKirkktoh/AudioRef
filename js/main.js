f92cd6eec88dfb80ad2f9d6ecd14ef0002e48e84
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2023-02-17 11:19:59 -0500

    finished jukebox
    finished jukebox

--- js/main.js
+++ js/main.js
@@ -2,15 +2,29 @@ console.log('JS is running!')
 
 // select all the placeholder images
 let albumCovers = document.querySelectorAll('.audio img');
+    audioEl = document.querySelector('audio'),
+    playButton = document.querySelector("#playButton")
+    rewindButton = document.querySelector("#rewindButton")
+    pauseButton = document.querySelector("#pauseButton")
 
-// play the audio
-function playTrack() {
-    //debugger;
+function loadAudio() {
+    audioEl.src = `audio/${this.dataset.trackref}.mp3`;
+    audioEl.load();
 
-    let currentAudio = document.querySelector(`audio[data-trackref="${this.dataset.trackref}"]`);
-    currentAudio.play();
+    playTrack();
 }
+// play the audio
+function playTrack() { audioEl.play(); }
+
+function pauseTrack() { audioEl.pause(); }
+
+function rewindTrack () { audioEl.currentTime = 0; }
 
 
 // add the reactions (the event we want to listen for)
-albumCovers.forEach(album => album.addEventListener('click',playTrack));
+albumCovers.forEach(album => album.addEventListener('click',loadAudio));
+
+// custom audio controls
+playButton.addEventListener('click', playTrack)
+rewindButton.addEventListener('click', rewindTrack)
+pauseButton.addEventListener('click', pauseTrack)
